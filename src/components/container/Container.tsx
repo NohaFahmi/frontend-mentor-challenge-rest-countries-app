91e7b39c7c4bd65bf45df59b8b210479ef5f0993
Noha M <nm1488@fayoum.edu.eg>
2022-07-08 17:51:12 +0200

    add the country card ui

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,18 +1,27 @@
-import './container.scss';
+import "./container.scss";
 import Header from "../header/Header";
 import FilterSearch from "../filter/Filter";
-import { Container, useColorModeValue, Box } from '@chakra-ui/react';
+import { Container, useColorModeValue, Box, Flex } from "@chakra-ui/react";
+import CountryCard from "../country-card/CountryCard";
 
 const AppContainer = () => {
   return (
-    <Box h='100%' w='100%' minH='100vh'>
-      <Header/>
- 
-      <Container maxW='100%' h='100%' minH='100vh' bg={useColorModeValue('grayLight', 'blueDark')}>
-      <FilterSearch/>
+    <Box h="100%" w="100%" minH="100vh">
+      <Header />
+
+      <Container
+        maxW="100%"
+        h="100%"
+        minH="100vh"
+        bg={useColorModeValue("grayLight", "blueDark")}
+      >
+        <FilterSearch />
+        <Flex gap="20px">
+          <CountryCard />
+          <CountryCard />
+        </Flex>
       </Container>
     </Box>
-    
   );
 };
 
--- src/components/country-card/CountryCard.tsx
+++ src/components/country-card/CountryCard.tsx
@@ -1,5 +1,91 @@
+import { useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Flex,
+  VStack,
+  HStack,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 const CountryCard = () => {
-    return <h1>Country Card</h1>
-}
+  return (
+    <VStack border="1px solid green" h="fit-content" w="300px" spacing="30px">
+      <Image
+        src="https://via.placeholder.com/150"
+        fallbackSrc="https://via.placeholder.com/120"
+        htmlWidth="100%"
+        h="180px"
+      />
+      <VStack align="start" w="100%" px="20px" pb="40px">
+        <Heading
+          as="h1"
+          size="md"
+          noOfLines={1}
+          color={useColorModeValue("blueDarker", "#fff")}
+          fontWeight="800"
+        >
+          Country Name
+        </Heading>
+        <VStack border="1px solid red" align="start" w="100%">
+          <HStack>
+            <Heading
+              as="h6"
+              size="sm"
+              noOfLines={1}
+              color={useColorModeValue("bluDark", "grayLight")}
+              fontWeight="600"
+            >
+              Population:
+            </Heading>
+            <Text
+              fontSize="sm"
+              color={useColorModeValue("grayDark", "grayDark")}
+              fontWeight="600"
+            >
+              (6xl) In love with React & Next
+            </Text>
+          </HStack>
+          <HStack>
+            <Heading
+              as="h6"
+              size="sm"
+              noOfLines={1}
+              color={useColorModeValue("bluDark", "grayLight")}
+              fontWeight="600"
+            >
+              Population:
+            </Heading>
+            <Text
+              fontSize="sm"
+              color={useColorModeValue("grayDark", "grayDark")}
+              fontWeight="600"
+            >
+              (6xl) In love with React & Next
+            </Text>
+          </HStack>
+          <HStack>
+            <Heading
+              as="h6"
+              size="sm"
+              noOfLines={1}
+              color={useColorModeValue("bluDark", "grayLight")}
+              fontWeight="600"
+            >
+              Population:
+            </Heading>
+            <Text
+              fontSize="sm"
+              color={useColorModeValue("grayDark", "grayDark")}
+              fontWeight="600"
+            >
+              (6xl) In love with React & Next
+            </Text>
+          </HStack>
+        </VStack>
+      </VStack>
+    </VStack>
+  );
+};
 
 export default CountryCard;
